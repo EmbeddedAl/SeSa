df9594e1e676c705901073d4bb6e004cd8f3c665
Albert Boeswald <al@boeswald.org>
2019-10-12 00:01:07 +0200

    Added some more implementation

---
+++ develop/logout.php
@@ -0,0 +1,33 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+       "http://www.w3.org/TR/html4/strict.dtd">
+
+<?php
+   /* always start session */
+   session_start();
+
+   /* destroy session */
+   session_destroy();
+
+   $_SESSION = array();
+?>
+<html>
+	<head>
+		<?php include ("layout/title.html"); ?>
+		<link rel="stylesheet" href="layout/style.css">
+	</head>
+
+	<body>
+		<div id="page">
+			<?php include ("layout/header.html"); ?>
+			<?php include ("layout/nav.html"); ?>
+
+         <div id="content">
+            <h2>Logout</h2>
+            <p>You have been logged out.</p>
+		  </div>
+
+		<?php include ("layout/footer.html"); ?>
+	</div>
+
+	</body>
+</html>
